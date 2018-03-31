0ba32adc4b54b5b9c3c7077cf2fece310d1b440a
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2018-03-31 12:09:45 +0100

    Fixed file header

--- src/HttpClient/Builder.php
+++ src/HttpClient/Builder.php
@@ -1,6 +1,17 @@
 <?php
 
-namespace Github\HttpClient;
+declare(strict_types=1);
+
+/*
+ * This file is part of Bitbucket API Client.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bitbucket\HttpClient;
 
 use Http\Client\Common\HttpMethodsClient;
 use Http\Client\Common\Plugin;
