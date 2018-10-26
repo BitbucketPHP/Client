7af9627721b6603668a05202757ea08848d487f6
Lucas Pekel <l.pekel@vrieling.nl>
2018-10-26 16:07:11 +0200

    Added create function for Comments. The api for bitbucket has changed through time and now it's possible to create a comment, hence this change.
    https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D/pullrequests/%7Bpull_request_id%7D/comments#post

--- src/Api/Repositories/Users/PullRequests/Comments.php
+++ src/Api/Repositories/Users/PullRequests/Comments.php
@@ -35,6 +35,20 @@ class Comments extends AbstractPullRequestsApi
     }
 
     /**
+     * @param array $params
+     *
+     * @throws \Http\Client\Exception
+     *
+     * @return array
+     */
+    public function create(array $params = [])
+    {
+        $path = $this->buildCommentsPath();
+
+        return $this->post($path, $params);
+    }
+
+    /**
      * @param string $comment
      * @param array  $params
      *
