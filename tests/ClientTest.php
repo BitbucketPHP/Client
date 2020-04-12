d3880e37420292c19267268c6958bc6a0a5652ae
Graham Campbell <graham@alt-three.com>
2020-04-12 15:49:25 +0100

    Added real test

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -31,4 +31,18 @@ class ClientTest extends TestCase
         $this->assertInstanceOf(Client::class, $client);
         $this->assertInstanceOf(HttpMethodsClient::class, $client->getHttpClient());
     }
+
+    public function testShowRepo()
+    {
+        $client = new Client();
+
+        $response = $client
+            ->repositories()
+            ->users('atlassian')
+            ->show('stash-example-plugin');
+
+        $this->assertIsArray($response);
+        $this->assertTrue(isset($response['uuid']));
+        $this->assertSame('{7dd600e6-0d9c-4801-b967-cb4cc17359ff}', $response['uuid']);
+    }
 }
