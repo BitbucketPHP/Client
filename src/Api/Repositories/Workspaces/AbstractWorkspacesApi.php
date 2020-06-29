3b0079edb08f236108fab5b9f47d87ed1c33427b
Nicolas Sanavia <bsn.sanavia.nicolas@gmail.com>
2020-06-29 16:06:06 +0200

    [NS] use interface

--- src/Api/Repositories/Workspaces/AbstractWorkspacesApi.php
+++ src/Api/Repositories/Workspaces/AbstractWorkspacesApi.php
@@ -15,6 +15,7 @@ namespace Bitbucket\Api\Repositories\Workspaces;
 
 use Bitbucket\Api\Repositories\AbstractRepositoriesApi;
 use Http\Client\Common\HttpMethodsClient;
+use Http\Client\Common\HttpMethodsClientInterface;
 
 /**
  * The abstract users api class.
@@ -33,11 +34,11 @@ abstract class AbstractWorkspacesApi extends AbstractRepositoriesApi
     /**
      * Create a new api instance.
      *
-     * @param \Http\Client\Common\HttpMethodsClient $client
+     * @param \Http\Client\Common\HttpMethodsClientInterface $client
      * @param string                                $username
      * @param string                                $repo
      */
-    public function __construct(HttpMethodsClient $client, string $username, string $repo)
+    public function __construct(HttpMethodsClientInterface $client, string $username, string $repo)
     {
         parent::__construct($client, $username);
         $this->repo = $repo;
