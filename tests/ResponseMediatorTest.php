7eb61c0bf6974bf4c610d86e52e9952f7866ed32
Graham Campbell <graham@alt-three.com>
2020-07-10 12:43:38 +0100

    Create ResponseMediatorTest.php

---
+++ tests/ResponseMediatorTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Bitbucket API Client.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bitbucket\Tests;
+
+use Bitbucket\HttpClient\Message\ResponseMediator;
+use Bitbucket\Exception\RuntimeException;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+class ResponseMediatorTest extends TestCase
+{
+    public function testGetContent()
+    {
+        $response = new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            \GuzzleHttp\Psr7\stream_for('{"foo": "bar"}')
+        );
+
+        $this->assertSame(['foo' => 'bar'], ResponseMediator::getContent($response));
+    }
+
+    public function testGetContentNotJson()
+    {
+        $body = 'foobar';
+        $response = new Response(
+            200,
+            [],
+            \GuzzleHttp\Psr7\stream_for($body)
+        );
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('The content type was not application/json.');
+
+        ResponseMediator::getContent($response);
+    }
+
+    public function testGetContentInvalidJson()
+    {
+        $body = 'foobar';
+        $response = new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            \GuzzleHttp\Psr7\stream_for($body)
+        );
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('json_decode error: Syntax error');
+
+        ResponseMediator::getContent($response);
+    }
+
+    public function testGetErrrorMessageInvalidJson()
+    {
+        $body = 'foobar';
+        $response = new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            \GuzzleHttp\Psr7\stream_for($body)
+        );
+
+        $this->assertNull(ResponseMediator::getErrorMessage($response));
+    }
+}
