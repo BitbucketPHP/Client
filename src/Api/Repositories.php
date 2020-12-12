505f4cfaf0d447ffb42faadcb4b3c956cab62b09
Graham Campbell <graham@alt-three.com>
2020-12-12 16:21:53 +0000

    Updated headers

--- src/Api/Repositories.php
+++ src/Api/Repositories.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Bitbucket API Client.
+ * This file is part of the Bitbucket API Client.
  *
  * (c) Graham Campbell <graham@alt-three.com>
  *
