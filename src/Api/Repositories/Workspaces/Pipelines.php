1aa58620be72d322ad5956c4bc1487e079582a4b
Michael Withagen <michael.withagen@live.com>
2025-10-17 11:08:48 +0200

    Add ssh namespace

--- src/Api/Repositories/Workspaces/Pipelines.php
+++ src/Api/Repositories/Workspaces/Pipelines.php
@@ -16,6 +16,7 @@ namespace Bitbucket\Api\Repositories\Workspaces;
 use Bitbucket\Api\Repositories\Workspaces\Pipelines\RemoteTriggers;
 use Bitbucket\Api\Repositories\Workspaces\Pipelines\Steps;
 use Bitbucket\HttpClient\Util\UriBuilder;
+use Bitbucket\Api\Repositories\Workspaces\PipelinesConfig\Ssh;
 
 /**
  * The pipelines API class.
@@ -63,7 +64,12 @@ class Pipelines extends AbstractWorkspacesApi
 
         return $this->post($uri, $params);
     }
-
+    
+    public function ssh(): Ssh
+    {
+        return new Ssh($this->getClient(), $this->workspace, $this->repo);
+    }
+    
     public function remoteTriggers(string $pipeline): RemoteTriggers
     {
         return new RemoteTriggers($this->getClient(), $this->workspace, $this->repo, $pipeline);
