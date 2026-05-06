94b5ef7c05e1b7dc474dcd1e9103dbce65483b7b
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-05-06 12:26:04 +0100

    Fix User-Agent version (#113)
    * Fix User-Agent version
    
    * Remove User-Agent test and changelog entry

--- src/Client.php
+++ src/Client.php
@@ -76,7 +76,7 @@ class Client
      *
      * @var string
      */
-    private const USER_AGENT = 'bitbucket-php-api-client/5.0';
+    private const USER_AGENT = 'bitbucket-php-api-client/5.1';
 
     private readonly Builder $httpClientBuilder;
     private readonly History $responseHistory;
